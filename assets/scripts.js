d46b0731c6779f852a4cec1730e0499d5b9a6239
Aaron Steward <aa.daly.stew@gmail.com>
2017-04-10 23:49:16 -0500

    finished DOM article

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,17 +1,14 @@
 addEventListener("load", function() {
-	addNavMenuListener();
+	document.getElementsByClassName("fa-bars")[0].addEventListener("click", toggleMenu);
 });
 
-function addNavMenuListener() {
-	nav_menu = document.getElementsByClassName("nav-menu")[0];
-	nav_menu.addEventListener("click", menuClicked);
-}
-
-function menuClicked() {
-	e.preventDefault();
-	var block_bott = document.getElementsByClassName("block-bott")[0];
-	var menu_shown = nav_menu.style.display;
-	if (menu_shown == "") {
-		menu_shown = window.
+function toggleMenu(e) {
+	e.preventDefault;
+	var block = document.getElementsByClassName("block-bott")[0];
+	if (block.style.display == "flex") {
+		block.style.display = "none"
+	} else {
+		block.style.display = "flex"
 	}
 }
+
